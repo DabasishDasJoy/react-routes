ef61265436db1be917252bc89e89f9d2ea70fdb5
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-02 00:16:31 +0600

    navigate using useNavigate Hook

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Friend.css";
 
 const Friend = ({ friend: { id, name, username, email } }) => {
+  const navigate = useNavigate();
+  const handleShowDetails = () => {
+    navigate(`/friend/${id}`);
+  };
   return (
     <div className="friend">
       <h3>{name}</h3>
       <p>{email}</p>
       <Link to={`/friend/${id}`}>{username}</Link>
+      <button onClick={handleShowDetails}>Show details</button>
     </div>
   );
 };
